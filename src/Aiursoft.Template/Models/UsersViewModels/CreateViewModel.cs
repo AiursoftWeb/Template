c4318b1c49e55f182b40be1f7839b8d93368a622
Anduin Xue <anduin@aiursoft.com>
2025-09-02 08:32:32 +0000

    Update display names in CreateViewModel and EditViewModel for consistency and clarity

--- src/Aiursoft.Template/Models/UsersViewModels/CreateViewModel.cs
+++ src/Aiursoft.Template/Models/UsersViewModels/CreateViewModel.cs
@@ -12,18 +12,18 @@ public class CreateViewModel: UiStackLayoutViewModel
     }
 
     [Required]
-    [Display(Name = "用户名")]
+    [Display(Name = "User name")]
     [ValidDomainName]
     public string? UserName { get; set; }
 
     [EmailAddress]
-    [Display(Name = "Email地址")]
+    [Display(Name = "Email Address")]
     [Required]
     public string? Email { get; set; }
 
     [Required]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     [DataType(DataType.Password)]
-    [Display(Name = "密码")]
+    [Display(Name = "Password")]
     public string? Password { get; set; }
 }
--- src/Aiursoft.Template/Models/UsersViewModels/EditViewModel.cs
+++ src/Aiursoft.Template/Models/UsersViewModels/EditViewModel.cs
@@ -12,22 +12,22 @@ public class EditViewModel: UiStackLayoutViewModel
     }
 
     [Required]
-    [Display(Name = "用户名")]
+    [Display(Name = "User name")]
     [ValidDomainName]
     public required string UserName { get; set; }
 
     [Required]
     [EmailAddress]
-    [Display(Name = "Email地址（用于登录）")]
+    [Display(Name = "Email Address")]
     public required string Email { get; set; }
 
     [DataType(DataType.Password)]
-    [Display(Name = "重置密码（不填不会修改密码）")]
+    [Display(Name = "Reset Password (leave empty to keep the same password)")]
     public string? Password { get; set; }
 
     [Required]
     public required string Id { get; set; }
 
-    [Display(Name = "具有管理员权限（能够修改数据库和审批课程）")]
+    [Display(Name = "Is Admin")]
     public bool IsAdmin { get; set; }
 }
