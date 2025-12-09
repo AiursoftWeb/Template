85cb781523d9362ab1d75a10fab33e27161b0c53
Anduin Xue <anduin@aiursoft.com>
2025-12-09 09:28:51 +0000

    Fix lint error: remove unused using directive in SqliteContext.cs

--- src/Aiursoft.Template.Sqlite/SqliteContext.cs
+++ src/Aiursoft.Template.Sqlite/SqliteContext.cs
@@ -1,6 +1,7 @@
 using Aiursoft.Template.Entities;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace Aiursoft.Template.Sqlite;
 
 public class SqliteContext(DbContextOptions<SqliteContext> options) : TemplateDbContext(options)
