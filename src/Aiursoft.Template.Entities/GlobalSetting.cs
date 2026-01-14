7eb8ce3b4c840023d61f01f56a3a9fa2ab7dd01e
Anduin Xue <anduin@aiursoft.com>
2026-01-14 15:41:54 +0000

    feat: Add new feature descriptions and refine existing ones on the dashboard and home pages, along with minor ReSharper annotations in GlobalSetting.cs.

--- src/Aiursoft.Template.Entities/GlobalSetting.cs
+++ src/Aiursoft.Template.Entities/GlobalSetting.cs
@@ -5,6 +5,9 @@ namespace Aiursoft.Template.Entities;
 public class GlobalSetting
 {
     [Key]
+    // ReSharper disable once EntityFramework.ModelValidation.UnlimitedStringLength
     public required string Key { get; set; }
+
+    // ReSharper disable once EntityFramework.ModelValidation.UnlimitedStringLength
     public string? Value { get; set; }
 }
